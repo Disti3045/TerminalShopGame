afe7d09519fdcf4d96755228821fee201a63ae10
LeonardoDiStefano <165501974+Disti3045@users.noreply.github.com>
2025-10-06 21:35:37 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-	
+	//Test cambiamento
 	public static void main(String[] args) {
 		//Inizializzazione Giocatore
 		Shopkeeper player = new Shopkeeper();
